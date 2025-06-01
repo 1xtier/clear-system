9cd1f5dbc466167b9c9591a537f0c9649025e7c9
1xtier <soraxsociety@gmail.com>
2025-06-01 09:58:48 +0400

    Edit

--- clear-system.sh
+++ clear-system.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-DT=$(date +"%Y-%m-%d %H:%M")
+DT=$(date +"%Y-%m-%d")
 LOG=/var/log/clear-system.log
 USERS=student
-killall -u $USERS
-echo "Clear system $DT" >> $LOG
-echo "__ Clear home dir User Admin" >> $LOG
-rm -rf /home/student
-echo "__ Copy backup dir user Admin" >> $LOG
-cp -r /opt/backup/student /home
-echo "Policity User Admin" >> $LOG
-chown -R student:student /home/student
-echo "----Exit-----" >> $LOG
+LOGFILE=$(tail -n 1 $LOG)
+
+
+clearlogfile() {
+	local LOG1=$(wc -l < $LOG)
+	local AA=10
+	if [ "$LOG1" -gt "$AA" ] 
+	then
+		sed -i '1,5d' $LOG
+	else 
+		echo "ok"
+	fi
+}
+clearsystem() {
+	killall -u $USERS
+	rm -rf /home/student
+	cp -r /opt/backup/student /home
+	chown -R student:student /home/student
+	echo "$DT" >> $LOG
+}
+
+
+if [ "$LOGFILE" == "$DT" ]; then 
+	echo "date - ok"
+else 
+	echo "date no"
+	clearsystem
+	clearlogfile
+fi
