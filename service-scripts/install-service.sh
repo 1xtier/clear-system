f0c354c1bada90683f27f5c315b0448dbd6b6377
1xtier <soraxsociety@gmail.com>
2025-06-07 21:37:10 +0400

    Edit usb script

--- service-scripts/install-service.sh
+++ service-scripts/install-service.sh
@@ -4,6 +4,8 @@ git clone https://github.com/1xtier/clear-system.git /opt/codos
 cp -r /opt/codos/cod-os /etc/
 cp -r /opt/codos/systemd/* /etc/systemd/system
 systemctl daemon-reload
+ln -s /etc/cod-os/usb.sh /usr/local/bin/usb
+
 }
 clone
 
